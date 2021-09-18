01fac92e6bb6a51919abf8c04ab6592240dbe5ea
cttalter <wselwsel@163.com>
2021-09-18 17:51:22 +0800

    segmented

--- homework3/Fibonacci.py
+++ homework3/Fibonacci.py
@@ -4,13 +4,14 @@ def fibonacci(x):
         return 0
     elif x == 1:
         return 1
-    else:
-        return fibonacci(x - 1) + fibonacci(x - 2)
+    return fibonacci(x - 1) + fibonacci(x - 2)
 
 
 if __name__ == '__main__':
     from datetime import datetime
+    import sys
 
+    sys.setrecursionlimit(60000)
     start = datetime.now()
-    print(fibonacci(35))
+    print(fibonacci(1000))
     print(datetime.now() - start)
--- homework3/Fibonacci_optimized.py
+++ homework3/Fibonacci_optimized.py
@@ -1,14 +1,20 @@
+record = {0: 0, 1: 1}  # record computed outcome
+
+
 def fibonacci(x):
     """计算Fibonacci数列第n项"""
-    record = {0: 0, 1: 1}  # record computed outcome
     if x not in record.keys():
         record[x] = fibonacci(x - 1) + fibonacci(x - 2)
     return record[x]
 
 
 if __name__ == '__main__':
-    from datetime import datetime
+    from datetime import datetime  # calculate time
+    import sys
 
+    sys.setrecursionlimit(2000000)
     start = datetime.now()
-    print(fibonacci(35))
+    for i in range(1, 301):
+        fibonacci(i * 2000)
+    print(fibonacci(600000))
     print(datetime.now() - start)
